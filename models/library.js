ee06fc3376d557118fc5678d756678a879372d8d
DimaYurashevich <dimaxik97@gmail.com>
2017-03-24 22:46:07 +0300

    start

---
+++ models/library.js
@@ -0,0 +1,11 @@
+module.exports=(Sequelize,sequelize)=>{
+    return sequelize.define('library',{
+        id:{
+            type:Sequelize.INTEGER,
+            primaryKey:true,
+            autoIncrement:true
+        },
+        title:Sequelize.STRING,
+        capacity:Sequelize.INTEGER
+    });
+};
